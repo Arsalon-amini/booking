d13d58c9717ad2fa37749323e0edead02e8f1758
arsalon <arsalonamini2020@gmail.com>
2021-06-09 17:48:46 +0800

    Database setup and installation using Mongodb

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 import express from "express"; 
 import {readdirSync} from "fs"; 
+import mongoose from "mongoose"; 
+import cors from 'cors'
 const morgan = require("morgan"); 
 require("dotenv").config();
 
 const app = express(); //express API encapsulated in app variable
 
+//db connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+    .then(()=> console.log('DB connected'))
+    .catch((err) => console.log('DB connection error', err)); 
+
 //middleware
+app.use(cors()); 
 app.use(morgan("dev")); //run morgan (HTTP request logger) in dev mode
 
 // automatically loads and applies routes as middleware
