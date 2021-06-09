1a5f90b56d710d0d39fee290a152dc5c7441a4aa
arsalon <arsalonamini2020@gmail.com>
2021-06-09 16:51:38 +0800

    autoloading routes using file system module from node

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
-import express from "express"; //using esm for import and export syntax
-import router from './routes/auth';
+import express from "express"; 
+import {readdirSync} from "fs"; 
 
 const app = express(); //express API encapsulated in app variable
 
-//route middleware
-app.use('/api', router); 
+//route middleware - automatically loads and applies routes as middleware
+readdirSync("./routes").map((r) => 
+    app.use("/api", require(`./routes/${r}`))
+); 
 
 app.listen(8000, () => console.log(`server running on port 8000`));
