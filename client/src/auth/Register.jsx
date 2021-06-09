964cdc0ad5aead2049de0c32fadec79e23840555
arsalon <arsalonamini2020@gmail.com>
2021-06-09 19:52:14 +0800

    Skeleton for Register Form

--- client/src/auth/Register.jsx
+++ client/src/auth/Register.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import {useState} from 'react'; 
 
 const Register = () => {
-    return(
-        <div class="container-fluid h1 p-5 text-center">
-            Register Page
-        </div>
+    const[name, setName] = useState(''); 
+    const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+
+    const handleSubmit = () => {
+        alert('send user info to backend')
+    }
+
+    const registerForm = () => (
+        <form onSubmit={handleSubmit}>
+            show the form
+            <button type="submit"> submit </button>
+        </form>
     )
+    return(
+        <>
+        <div class="container-fluid  bg-secondary p-5 text-center">
+            <h1>Register</h1>
+        </div>
+
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6" offset-md-3>
+                    {registerForm()}
+                </div>
+            </div>
+        </div>
+        </>
+    );
 }
 
 export default Register; 
