a944b55a58617f8b8664e4a06502cbe1b659ef65
arsalon <arsalonamini2020@gmail.com>
2021-06-09 20:37:03 +0800

    Axios added for posting register form data to backend API

--- client/src/auth/Register.jsx
+++ client/src/auth/Register.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import {useState} from 'react'; 
 import RegisterForm from '../components/RegisterForm'; 
+import axios from 'axios'; 
 
 const Register = () => {
     const[name, setName] = useState(''); 
     const[email, setEmail] = useState('');
     const[password, setPassword] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault(); 
-        console.table({name, email, password});
-    }
+        try {
+            const res = await axios.post(`http://localhost:8000/api/register`, {
+                name: name, 
+                email: email,
+                password: password
+            });
+            console.log("Register User ===> ", res); 
+        } catch (error) {
+            console.log(error); 
+        }
+    };
 
 
     return(
@@ -18,7 +28,6 @@ const Register = () => {
         <div class="container-fluid  bg-secondary p-5 text-center">
             <h1>Register</h1>
         </div>
-
         <div className="container">
             <div className="row">
                 <div className="col-md-6" offset-md-3>
