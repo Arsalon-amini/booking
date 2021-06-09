5d625e07a004610103ed655f60d9cd025816f467
arsalon <arsalonamini2020@gmail.com>
2021-06-09 16:42:57 +0800

    Basic Express Module Setup

---
+++ server/routes/auth.js
@@ -0,0 +1,9 @@
+import express from 'express'; 
+
+const router = express.Router(); //gives access to express router
+
+router.get('/:message', (req, res) => {
+    res.status(200).send(req.params.message); 
+});
+
+export default router; 
--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-console.log("node server running changed...", process)
+import express from "express"; //using esm for import and export syntax
+import router from './routes/auth';
+
+const app = express(); //express API encapsulated in app variable
+
+//route middleware
+app.use('/api', router); 
+
+app.listen(8000, () => console.log(`server running on port 8000`));
