62da4be1f6bee8159e0a0f67665b5ca9d2ac617e
arsalon <arsalonamini2020@gmail.com>
2021-06-12 19:30:32 +0800

    Login route w/ controlling logic outline

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,16 +1,18 @@
 import User from '../models/user'; 
 
 export const register = async (req, res) => {
+    console.log(req.body);
     const {name, email, password} = req.body;  
     
     //validation
-    if(!name) 
-        return res.status(400).send('Name is required'); 
+    if(!name) return res.status(400).send('Name is required'); 
     
     if(!password || password.length < 6) 
-        return res.status(400).send('Password is required and should be min 6 characters long'); 
+        return res
+            .status(400)
+            .send('Password is required and should be min 6 characters long'); 
 
-    let userExist = await User.findOne({email}).exec(); 
+    let userExist = await User.findOne({ email }).exec(); 
     if(userExist) return res.status(400).send("Email is already taken"); 
 
     //register
@@ -20,7 +22,11 @@ export const register = async (req, res) => {
         console.log('User created', user);
         return res.json({ ok: true});
     } catch (err) {
-        console.log("create user error"); 
-        return res.status(400).send('Error, try again'); 
+        console.log("create user error", err); 
+        return res.status(400).send('Something Unexpected happened'); 
     }
+};
+
+export const login = async (req, res) => {
+    console.log(req.body); 
 }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,9 +3,10 @@ import express from 'express';
 const router = express.Router(); //gives access to express router
 
 //controllers
-import { register } from '../controllers/auth';
+import { register, login } from '../controllers/auth';
 
 
 router.post('/register', register);
+router.post('/login', login); 
 
 module.exports = router;
