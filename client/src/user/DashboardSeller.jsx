7cdaef2cfaa36c40f4fa81cfe3ebab05a8441ddd
arsalon <arsalonamini2020@gmail.com>
2021-06-17 15:25:42 +0800

    Dashboard (sellers) - connect to stripe outline for connecting to stripe to recieve payouts for earnings

--- client/src/user/DashboardSeller.jsx
+++ client/src/user/DashboardSeller.jsx
@@ -2,6 +2,7 @@ import DashboardNav from '../components/DashboardNav';
 import ConnectNav from '../components/ConnectNav';
 import { Link } from 'react-router-dom';
 import {useSelector} from 'react-redux'; 
+import { HomeOutlined } from '@ant-design/icons'
 
 const DashboardSeller = () => {
     const { auth } = useSelector((state) => ({...state}));
@@ -21,8 +22,21 @@ const DashboardSeller = () => {
     const notConnected = () => (
         <div className="container-fluid">
             <div className="row">
-                <div className="col-md-10">
-                    <h2>Connect With Stripe</h2>
+                <div className="col-md-6 offset-md-3 text-center">
+                    <div className="p-5 pointer">
+                        <HomeOutlined className="h1" />
+                        <h4>Setup Payouts to post hotel rooms</h4>
+                        <p className="lead">We partner with stripe to transfer earnings to your bank account </p>
+                        <button className="btn btn-primary mb-3">
+                            Setup Payouts
+                        </button>
+                            <p className="text-muted">
+                                <small>
+                                    You'll be re-directed to stripe to complete the onboarding process
+                                </small>
+                                
+                            </p>
+                    </div>
                 </div>
             </div>
         </div>
