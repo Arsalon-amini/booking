19183318d9c91157e1443bf10c135bebc5f1d41a
arsalon <arsalonamini2020@gmail.com>
2021-06-25 21:16:26 +0800

    Get Account Balance from Stripe (server side)

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post("/create-connect-account", requireSignIn, createConnectAccount); 
 router.post('/get-account-status', requireSignIn, getAccountStatus);
+router.post('/get-account-balance', requireSignIn, getAccountBalance);
 
 module.exports = router; 
